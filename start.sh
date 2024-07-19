8ca033c7668f1d3e21c937cc5287355b45ac52f3
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-07-19 10:48:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git --version 
-git clone https://github.com/Tamilupdates/HHStreamENVBot.git stream-bot
+git clone http://github.com/Tamilupdates/HHStreamENVBot.git stream-bot
 cd stream-bot
 python -m WebStreamer
