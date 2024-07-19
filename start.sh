993be3006da727b75a6ca53104410fed50297bcd
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-07-19 11:08:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git --version 
-git clone https://github.com/Tamilupdates/HHStreamENVBot.git tgfs
+git clone http://github.com/Tamilupdates/HHStreamENVBot tgfs
 cd tgfs
 python -m WebStreamer
